061d940ab17525db7b49640dcdd3ed6049486060
Peter Anema <7320173+PeterAnema@users.noreply.github.com>
2021-09-16 19:13:09 +0200

    Update

--- opdracht_namen/src/MainClass.java
+++ opdracht_namen/src/MainClass.java
@@ -1,3 +1,7 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainClass {
@@ -5,14 +9,22 @@ public class MainClass {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < 5; i++) {
+        List<String> names = new ArrayList<>();
+
+        // Get names
+        while (true) {
             System.out.print("Geef een naam: ");
-            String naam = scanner.nextLine();
+            String name = scanner.nextLine();
+            if (name.isEmpty())
+                break;
+            names.add(name);
+        };
 
-
+        // Print names
+        System.out.println("De ingevoerde namen zijn:");
+        for (String name: names) {
+            System.out.println(name);
         }
-
-
 
     }
 
